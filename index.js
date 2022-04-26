1651710528d19f4a969a39786fa810d98d1ed95d
tkyg <80114433+tkyg@users.noreply.github.com>
2022-04-26 15:21:41 -0400

    commit tree 0f0bbf8f5c44b29de8540269af05690f9ad5cc65

---
+++ index.js
@@ -0,0 +1,22 @@
+// const divide = (param1, param2) => 2000/100;
+
+// const square = x => x * 2;
+
+// const add = (a, b) => a + b;
+
+
+
+
+
+
+
+
+const divide = (a, b) => (2000/100)
+
+const square = x => x * x
+
+const add = (a, b) => a + b
+
+
+
+
